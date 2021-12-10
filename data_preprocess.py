12ff6ef9f024e507056cd7fbe2546a179c8577f9
vidyasagar150797 <vidyasagarsasumana1997@gmail.com>
2021-12-10 08:38:44 +0530

    This is the second version of data preprocessing

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,3 +3,9 @@ def data_preprocess():
 
 data_preprocess = data_preprocess()
 print(data_preprocess)
+
+def data_preprocess_2():
+    return "This is a data preprocessed code version 2"
+
+data_preprocess_2 = data_preprocess_2()
+print(data_preprocess_2)

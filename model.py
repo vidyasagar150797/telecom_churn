3873f2ca861638aa3b46cc1306cbb1f6b0ea6074
vidyasagar150797 <vidyasagarsasumana1997@gmail.com>
2021-12-10 13:40:30 +0530

    added three different models

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("This is the Model.py code")
+
+def model1():
+    print("this is model one")
+
+def model2():
+    print("this is model two")
+
+def model3():
+    print("this is model three")
